a7c2e3a9abae0884e255dc85e670c96b34d76dc0
Nick <nfz32@comcast.net>
2019-05-23 16:50:16 -0700

    add material ui snackbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import Slider from 'rc-slider/lib/Slider';
 import 'rc-slider/assets/index.css';
 import './NavBar.css'
@@ -9,19 +12,30 @@ class NavBar extends Component {
 	constructor(props){
 		super(props);
 		this.state= {
-			format: 'hex'
+			format: 'hex',
+			open: false,
 		}
 	}
-	handleChange =(e) => {
+	handleFormatChange =(e) => {
 		let value = e.target.value;
 		this.setState({
-			format: value
+			...this.state,
+			format: value,
+			open:true,
 		})
 		this.props.handleChange(value)
 	}
+
+	closeSnackbar = () => {
+		this.setState({
+			...this.state,
+			open: false
+		})
+	}
+
 	render() {
 		const { level, changeLevel } = this.props;
-		const { format } = this.state;
+		const { format, open } = this.state;
 		return (
 			<div className='Navbar'>
 				<div className='logo'>
@@ -34,12 +48,27 @@ class NavBar extends Component {
 					</div>
 				</div>
 				<div className="select-container">
-					<Select value={format} onChange={this.handleChange}>
+					<Select value={format} onChange={this.handleFormatChange}>
 						<MenuItem value='hex'>HEX - #fffff</MenuItem>
 						<MenuItem value='rgb'>RGB - rgb(255, 255, 255)</MenuItem>
 						<MenuItem value='rgba'>RGBA - rgb(255, 255, 255, 1.0)</MenuItem>
 					</Select>
 				</div>
+				<Snackbar 
+					anchorOrigin={{vertical: 'bottom', horizontal: 'left'}} 
+					open={open}
+					autoHideDuration={3000}
+					message={<span id='message-id'>Format Changed to {format.toUpperCase()}</span>}
+					ContentProps={{
+						"aria-describedby": "message-id"
+					}}
+					onClose={this.closeSnackbar}
+					action={[
+						<IconButton onClick={this.closeSnackbar}>
+							<CloseIcon/>
+						</IconButton>
+					]}
+				/>
 			</div>
 		)
 	}
