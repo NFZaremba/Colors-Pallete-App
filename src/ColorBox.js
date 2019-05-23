45cd5e4bcfc7b4abbde09ca7ee61e7603c4f859d
Nick <nfz32@comcast.net>
2019-05-23 14:35:59 -0700

    add color helper

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { CopyToClipboard  } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
-export default class ColorBox extends Component {
+ class ColorBox extends Component {
+	 constructor(props){
+		 super(props);
+		 this.state={
+			 copied:false
+		 }
+	 }
+
+	 changeCopyState =() => {
+		 this.setState({
+			 copied:true
+		 }, () => {
+			 setTimeout(() => this.setState({ copied: false}), 1500)
+		 })
+	 }
+
 	render() {
 		const{ name, background } = this.props;
+		const { copied } = this.state;
 		return (
+			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 			<div style={{background: background}} className='ColorBox'>
+				<div style={{background: background}} className={`copy-overlay ${copied && "show"}`}/>
+				<div className={`copy-msg ${copied && "show"}`}>
+					<h1>copied!</h1>
+					<p>{this.props.background}</p>
+				</div>
 				<div className="copy-container">
 					<div className="box-content">
 						<span>{name}</span>
@@ -14,6 +37,9 @@ export default class ColorBox extends Component {
 				</div>
 				<span className='see-more'>More</span>
 			</div>
+			</CopyToClipboard >
 		)
 	}
 }
+
+export default ColorBox;
